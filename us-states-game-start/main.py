47fdf65aab871ee1303b3f84650ec7b0b4a38d78
msuneth <msuneth@gmail.com>
2024-02-16 06:02:36 +0530

    us-state-quiz-map loaded

---
+++ us-states-game-start/main.py
@@ -0,0 +1,10 @@
+from turtle import Turtle,Screen
+
+screen = Screen()
+screen.title("U.S. States Game")
+image = "blank_states_img.gif"
+screen.addshape(image)
+turtle = Turtle(image)
+
+
+screen.exitonclick()
